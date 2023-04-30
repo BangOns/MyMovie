bdcb179f74b4cb46c76d80b4b9025fde6e92b5ba
BangOns <syahr9950@gmail.com>
2023-04-30 20:54:37 +0700

    clear navbar List

---
+++ src/Components/html/ComponentsNavBar/GetDataGenre.jsx
@@ -0,0 +1,5 @@
+export const movieGenreName = ["Action", "Drama", "Comedy", "Crime", "Horror"];
+export const movieGenreID = [28, 18, 35, 80, 36];
+
+export const tvGenreName = ["Action", "Drama", "Comedy", "Crime", "Horror"];
+export const tvGenreID = [10759, 18, 35, 80, 9648];
